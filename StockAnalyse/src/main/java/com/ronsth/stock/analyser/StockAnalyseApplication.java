64200febbca7c0474c423d0bd0fedea17000acb7
Rohan Sheth <rjsheth1593@gmail.com>
2021-08-31 23:03:25 +0530

    initial commit

---
+++ StockAnalyse/src/main/java/com/ronsth/stock/analyser/StockAnalyseApplication.java
@@ -0,0 +1,13 @@
+package com.ronsth.stock.analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalyseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalyseApplication.class, args);
+	}
+
+}
